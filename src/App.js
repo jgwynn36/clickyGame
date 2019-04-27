815050af764981fdc763f7f534ec74f77fb321e5
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-04-26 22:18:18 -0400

    app component added

---
+++ src/App.js
@@ -0,0 +1,6 @@
+//import dependencies
+import React, { Component } from "react"; 
+import Navbar from  "./components/Navbar"; 
+import Header from "./components/Wrapper"; 
+import Footer from "./components/Footer";
+import cartoon from ".cartoon.json"; 
---
+++ src/index.js
@@ -0,0 +1,8 @@
+//import dependencies
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import './index.css'; 
+import App from './App'; 
+
+//Render app
+ReactDOM.render(<App />, document.getElementById("root")); 
