bebc773491b9df3d2f2a9523747312e5ae424184
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.attlocal.net>
2019-04-28 19:32:50 -0400

    footer component created

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"; 
+import "./Footer.css"; 
+
+const Footer = () => {
+    <header className = "footer" > 
+    <p>Memory Game created by J Gwynn</p>
+    </header>
+}; 
+
+export default Footer; 
